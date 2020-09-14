5fe1371c9351acc0e3db8c3de005f038d23045dc
Chris Touchton <christouchton@gmail.com>
2020-09-14 10:19:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function dwarfRollCall(dwarves) {
-}
+ const dwarfNames = []
+ for (let i = 0; i < dwarves.length; i++){
+   dwarfNames.push(`${i+1}. ${dwarves[i]} `)
+ }
+ return `${dwarfNames.join(' ')}`
 
 function summonCaptainPlanet(planeteerCalls){
 }
