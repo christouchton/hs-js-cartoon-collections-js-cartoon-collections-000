423e7dfda180104dccf94eaa189b1397b460437c
Chris Touchton <christouchton@gmail.com>
2020-10-06 15:35:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 function dwarfRollCall(dwarves) {
- const dwarfNames = []
- for (let i = 0; i < dwarves.length; i++){
-   dwarfNames.push(`${i+1}. ${dwarves[i]} `)
+ const dwarfNames = [];
+ for (let i = 0; i < dwarves.length; i++) {
+   dwarfNames.push(`${i+1}. ${dwarves[i]} `);
  }
- return `${dwarfNames.join(' ')}`
+ return dwarfNames.join("");
+} 
 
 function summonCaptainPlanet(planeteerCalls){
+  const yellPlaneteerCalls = [];
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    const exclam = "!";
+    yellPlaneteerCalls.push(`${planeteerCalls[i]}${exclam}`.toUpperCase());
+ }
+return yellPlaneteerCalls;
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true;
+    }
+ }
+ return false;
 }
 
 function findTheCheese (foods) {
-}
+  for (let i = 0; i < foods.length; i++) {
+    if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
+      return foods[i];
+    }
+  }
+  return "no cheese!";
+}  
